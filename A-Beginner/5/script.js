f6b07cd90623f419953235b0cc1536be558a99ed
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-10-12 14:39:59 +0200

    5

---
+++ A-Beginner/5/script.js
@@ -0,0 +1,35 @@
+// 5. Deklarera en konstant variabel och tilldela den ett värde.
+
+// gammalt sätt 
+// var x = 5 
+
+// // konstant variabel
+// const Y = 7
+
+// // icke-konstant vaiabel
+// let = 7
+
+const x = 100;
+
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
